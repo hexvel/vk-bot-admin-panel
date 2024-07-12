c8b67ff86edfeb2c3abbdd5b82c7399ca329384e
Hexvel <dimaabdukarimov1234@gmail.com>
2024-07-12 21:52:40 +0300

    test

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import RequestCode from '../components/RequestCode'
 const AdminPanel: React.FC = () => {
 	return (
 		<div className='h-full p-8 flex flex-col justify-center items-center'>
-			<h1 className='text-3xl font-bold mb-8'>Панель управления модулем</h1>
+			<h1 className='text-3xl font-bold mb-8'>Панель управления модулем 123</h1>
 			<div className='w-full p-4 md:w-[600px] shadow-2xl shadow-violet-950 border border-gray-600 rounded-xl md:p-10'>
 				<RequestCode />
 				<TokenUpdate />
